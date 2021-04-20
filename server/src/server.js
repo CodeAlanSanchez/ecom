a273b1936faf81f6e70a8e6e5ad8cdfe604bc945
Alan Sanchez <realalansan@gmail.com>
2021-04-20 10:23:56 -0700

    Add product route

--- server/src/server.js
+++ server/src/server.js
@@ -3,19 +3,23 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+import productRoute from './routes';
+
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
-app.use(express.json({ limit: "2mb" }));
-app.use(express.urlencoded({ limit: "2mb", extended: true}));
+app.use(express.json({ limit: '2mb' }));
+app.use(express.urlencoded({ limit: '2mb', extended: true }));
 
-const {CONNECTION_URI} = process.env;
+app.use('/api', productRoute);
+
+const { CONNECTION_URI } = process.env;
 const PORT = 5000 || process.env.PORT;
 
-mongoose.connect(CONNECTION_URI, {useNewUrlParser: true, useUnifiedTopology: true})
-    .then(() => app.listen(PORT, () => console.info(`Server running on port ${PORT}`)))
-    .catch((error) => console.error(error));
+mongoose.connect(CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.info(`Server running on port ${PORT}`)))
+  .catch((error) => console.error(error));
 
 mongoose.set('useFindAndModify', false);

5efb2baf4dfb1614026ea972f034ba9d0a416f36
Alan Sanchez <realalansan@gmail.com>
2021-04-21 13:52:10 -0700

    Add routes to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Store from './components/Store/Store';
+import Checkout from './components/Checkout/Checkout';
+import ProductDetails from './components/ProductDetails/ProductDetails';
 
 const App = () => (
-  <div />
+  <div>
+    <main>
+      <Switch>
+        <Route path={['/', '/home']} exact component={<Store />} />
+        <Route path={['/checkout']} component={<Checkout />} />
+        <Route path={['/product']} component={<ProductDetails />} />
+      </Switch>
+    </main>
+  </div>
 );
 
 export default App;

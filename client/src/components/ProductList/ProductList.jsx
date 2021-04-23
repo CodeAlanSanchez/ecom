50426c7aec6b8840ac75e4f6a466148d5b11bf5f
Alan Sanchez <realalansan@gmail.com>
2021-04-22 17:54:25 -0700

    Add ProductList component

---
+++ client/src/components/ProductList/ProductList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Product from '../Product/Product';
+import './styles.css';
+
+const ProductList = () => (
+  <div className="product-list">
+    <Product />
+    <Product />
+    <Product />
+  </div>
+);
+
+export default ProductList;

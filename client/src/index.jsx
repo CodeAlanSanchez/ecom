a9d9a18adcf5e7c29c4ddc874abe015896dd0e39
Alan Sanchez <realalansan@gmail.com>
2021-04-22 17:53:46 -0700

    Add BrowserRouter

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,12 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './styles.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'),
+);

ddc9eb0a1f244bef15c63d024bb6fce0f488ce62
Alan Sanchez <realalansan@gmail.com>
2021-04-22 17:54:07 -0700

    Add product component with styles

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import './styles.css';
 
 const Product = () => (
-  <div />
+  <a href="https://www.google.com">
+    <div className="product">
+      <img alt="product" />
+      <h5>Athletic Shoes</h5>
+      <h6>Nike</h6>
+      <p>A pair of shoes which can be used to run in.</p>
+    </div>
+  </a>
 );
 
 export default Product;

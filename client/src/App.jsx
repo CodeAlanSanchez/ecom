d46ca8098dbda1640ddc9574ead8af3e5ce65720
Alan Sanchez <realalansan@gmail.com>
2021-04-22 17:53:36 -0700

    Fix component in Route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,14 +3,16 @@ import { Route, Switch } from 'react-router-dom';
 import Store from './components/Store/Store';
 import Checkout from './components/Checkout/Checkout';
 import ProductDetails from './components/ProductDetails/ProductDetails';
+import Header from './components/Header/Header';
 
 const App = () => (
   <div>
+    <Header />
     <main>
       <Switch>
-        <Route path={['/', '/home']} exact component={<Store />} />
-        <Route path={['/checkout']} component={<Checkout />} />
-        <Route path={['/product']} component={<ProductDetails />} />
+        <Route path={['/', '/home']} exact component={Store} />
+        <Route path={['/checkout']} component={Checkout} />
+        <Route path={['/product']} component={ProductDetails} />
       </Switch>
     </main>
   </div>

8130cdc2671e01b049b93b2887d8debae7ee713c
Alan Sanchez <realalansan@gmail.com>
2021-04-22 17:54:38 -0700

    Add placeholder ProductList

--- client/src/components/Store/Store.jsx
+++ client/src/components/Store/Store.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import ProductList from '../ProductList/ProductList';
 
 const Store = () => (
-  <div />
+  <div>
+    <ProductList />
+  </div>
 );
 
 export default Store;

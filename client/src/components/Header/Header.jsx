ca951b541002e9d393af9aa97ae02f405b31db69
Alan Sanchez <realalansan@gmail.com>
2021-04-23 09:20:31 -0700

    Add header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import './styles.css';
 
 const Header = () => (
-  <div />
+  <nav>
+    <ul>
+      <li>
+        <a href="w">
+          <h3>
+            Home
+          </h3>
+        </a>
+      </li>
+      <li>
+        <a href="w">About</a>
+      </li>
+    </ul>
+  </nav>
 );
 
 export default Header;

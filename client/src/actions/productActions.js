079d18e3881970ed7d2460414e78358b7737cb0c
Alan Sanchez <realalansan@gmail.com>
2021-04-27 18:09:44 -0700

    Add constants

--- client/src/actions/productActions.js
+++ client/src/actions/productActions.js
@@ -1,10 +1,13 @@
 import * as api from '../api';
+import {
+  FETCH_PRODUCTS, CREATE_PRODUCT, UPDATE_PRODUCT, REMOVE_PRODUCT,
+} from '../constants/actionTypes';
 
 export const getProducts = () => async (dispatch) => {
   try {
     const { data } = api.fetchProducts();
 
-    dispatch({ type: 'FETCH_PRODUCTS', payload: data });
+    dispatch({ type: FETCH_PRODUCTS, payload: data });
   } catch (error) {
     console.error(error);
   }
@@ -14,7 +17,7 @@ export const createProduct = (product) => async (dispatch) => {
   try {
     const { data } = api.createProduct(product);
 
-    dispatch({ type: 'CREATE_PRODUCT', payload: data });
+    dispatch({ type: CREATE_PRODUCT, payload: data });
   } catch (error) {
     console.error(error);
   }
@@ -24,7 +27,7 @@ export const updateProduct = (product, id) => async (dispatch) => {
   try {
     const { data } = api.updateProduct(product, id);
 
-    dispatch({ type: 'UPDATE_PRODUCT', payload: data });
+    dispatch({ type: UPDATE_PRODUCT, payload: data });
   } catch (error) {
     console.error(error);
   }
@@ -34,7 +37,7 @@ export const deleteProduct = (id) => async (dispatch) => {
   try {
     const { data } = api.deleteProduct(id);
 
-    dispatch({ type: 'DELETE_PRODUCT', payload: data });
+    dispatch({ type: REMOVE_PRODUCT, payload: data });
   } catch (error) {
     console.error(error);
   }

cf84666c152978944f0ffda8c21108c592322cbd
Alan Sanchez <realalansan@gmail.com>
2021-04-28 22:28:02 -0700

    Add Cart component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Store from './components/Store/Store';
 import Checkout from './components/Checkout/Checkout';
 import ProductDetails from './components/ProductDetails/ProductDetails';
 import Header from './components/Header/Header';
+import Cart from './components/Cart/Cart';
 
 const App = () => (
   <div>
@@ -12,6 +13,7 @@ const App = () => (
       <Switch>
         <Route path={['/', '/home']} exact component={Store} />
         <Route path={['/checkout']} component={Checkout} />
+        <Route path={['/cart']} component={Cart} />
         <Route path={['/product']} component={ProductDetails} />
       </Switch>
     </main>

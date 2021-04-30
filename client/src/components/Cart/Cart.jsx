1298728575763798a747c24d2d27fe9018980456
Alan Sanchez <realalansan@gmail.com>
2021-04-29 20:44:52 -0700

    Edit Cart

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Cart = () => (
+const Cart = ({ items }) => (
   <div>
     <h1>Cart</h1>
-    <p className="price">23432</p>
+    {items.map((item) => (
+      <div key={item._id}>
+        <h3>{item.name}</h3>
+        <h3>{item.amount}</h3>
+      </div>
+    ))}
   </div>
 );
 

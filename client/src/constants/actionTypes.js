f782cfc91f935441eefa434c5e51924ea1faa5c7
Alan Sanchez <realalansan@gmail.com>
2021-05-06 05:11:00 -0700

    Add cart constants

--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -3,3 +3,7 @@ export const UPDATE_PRODUCT = 'UPDATE_PRODUCT';
 export const REMOVE_PRODUCT = 'REMOVE_PRODUCT';
 export const FETCH_PRODUCTS = 'FETCH_PRODUCTS';
 export const FETCH_PRODUCT = 'FETCH_PRODUCT';
+
+export const FETCH_CART = 'FETCH_CART';
+export const ADD_TO_CART = 'ADD_TO_CART';
+export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';

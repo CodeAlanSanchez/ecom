cbbf8ca9fecaa3503e64769b6d8beb6849ca62e1
Alan Sanchez <realalansan@gmail.com>
2021-05-06 22:14:27 -0700

    Add remove and add actions

--- client/src/actions/cartActions.js
+++ client/src/actions/cartActions.js
@@ -4,3 +4,5 @@ import {
 } from '../constants/actionTypes';
 
 export const getCart = () => ({ type: FETCH_CART });
+export const addToCart = (product) => ({ type: ADD_TO_CART, payload: product });
+export const removeFromCart = (id) => ({ type: REMOVE_FROM_CART, payload: id });

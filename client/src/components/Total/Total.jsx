abc95fad226cd96c8852a7ea2847dac451f515a7
Alan Sanchez <realalansan@gmail.com>
2021-05-07 23:18:42 -0700

    Create Total.jsx mockup

---
+++ client/src/components/Total/Total.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './total.module.css';
+
+const Total = () => (
+  <div className={styles.total}>
+    <h1>Summary</h1>
+    <hr />
+    <h5>Athletic Shoes x 1</h5>
+    <p>$59.99</p>
+  </div>
+);
+
+export default Total;

37b4cd5c4e2188f8417c876bebee8df001e1fc0b
Alan Sanchez <realalansan@gmail.com>
2021-05-07 19:42:30 -0700

    Add initial product fetch dispatch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 import Store from './components/Store/Store';
 import Checkout from './components/Checkout/Checkout';
 import ProductDetails from './components/ProductDetails/ProductDetails';
 import Header from './components/Header/Header';
 import Cart from './components/Cart/Cart';
+import { getProducts } from './actions/productActions';
 
-const App = () => (
-  <div>
-    <Header />
-    <main>
-      <Switch>
-        <Route path={['/', '/home']} exact component={Store} />
-        <Route path={['/checkout']} component={Checkout} />
-        <Route path={['/cart']} component={Cart} />
-        <Route path={['/product']} component={ProductDetails} />
-      </Switch>
-    </main>
-  </div>
-);
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <Switch>
+          <Route path={['/', '/home']} exact component={Store} />
+          <Route path={['/checkout']} component={Checkout} />
+          <Route path={['/cart']} component={Cart} />
+          <Route path={['/product']} component={ProductDetails} />
+        </Switch>
+      </main>
+    </div>
+  );
+};
 
 export default App;

345e9350889683f9b6363a2665d906ee1895befc
Alan Sanchez <realalansan@gmail.com>
2021-05-07 23:17:58 -0700

    Fix URL

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000';
+const url = 'http://localhost:5000/api/products';
 
-export const fetchProducts = () => axios.get(`${url}/api`);
-export const createProduct = (product) => axios.post(`${url}/api`, product);
-export const updateProduct = (product, id) => axios.patch(`${url}/api/${id}`, product);
-export const deleteProduct = (id) => axios.delete(`${url}/api/${id}`);
+export const fetchProducts = () => axios.get(`${url}`);
+export const createProduct = (product) => axios.post(`${url}`, product);
+export const updateProduct = (product, id) => axios.patch(`${url}/${id}`, product);
+export const deleteProduct = (id) => axios.delete(`${url}/${id}`);

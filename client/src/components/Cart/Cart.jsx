38671c12ae027cd851a9d120aabcc5ade44d7ece
Alan Sanchez <realalansan@gmail.com>
2021-05-07 19:46:01 -0700

    Get items from redux store

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Cart = ({ items }) => (
-  <div>
-    <h1>Cart</h1>
-    {items.map((item) => (
-      <div key={item._id}>
-        <h3>{item.name}</h3>
-        <h3>{item.amount}</h3>
-      </div>
-    ))}
-  </div>
-);
+const Cart = () => {
+  const items = useSelector((state) => state.cart);
+
+  return (
+    <div>
+      <h1>Cart</h1>
+      {items.map((item) => (
+        <div key={item._id}>
+          <h3>{item.name}</h3>
+          <h3>{item.amount}</h3>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Cart;

995ae66c9be751d3feb56958943da10e35fd0471
Alan Sanchez <realalansan@gmail.com>
2021-05-10 11:14:25 -0700

    Add props

--- client/src/components/Total/Total.jsx
+++ client/src/components/Total/Total.jsx
@@ -1,13 +1,33 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import { useSelector } from 'react-redux';
 import styles from './total.module.css';
 
-const Total = () => (
-  <div className={styles.total}>
-    <h1>Summary</h1>
-    <hr />
-    <h5>Athletic Shoes x 1</h5>
-    <p>$59.99</p>
-  </div>
-);
+const Total = () => {
+  const items = useSelector((state) => state.cart);
+
+  return (
+    <div className={styles.total}>
+      <h1>Summary</h1>
+      <hr />
+      {items.map((item) => (
+        <div className={styles.item}>
+          <h5>{item.name}</h5>
+          <p className={styles.price}>{item.price}</p>
+        </div>
+      ))}
+      <hr />
+      <p>
+        <b>
+          Total:
+          {' '}
+        </b>
+        $
+        {items.reduce((acc, curr) => curr.price + acc, 0)}
+      </p>
+      <button type="button">Checkout</button>
+    </div>
+  );
+};
 
 export default Total;

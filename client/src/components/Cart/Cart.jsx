c1047506428fe8f9f5dcb74e1fe5b10986ae1eb8
Alan Sanchez <realalansan@gmail.com>
2021-05-10 20:49:08 -0700

    Refactor JSX

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -12,32 +12,34 @@ const Cart = () => {
         <div className={styles.heading}>
           <h1>Cart</h1>
         </div>
-        {items.length <= 0
-          ? <h5 className={styles.empty}>Cart is empty...</h5>
-          : (
-            <div className={styles.cartContainer}>
-              {items.map((item) => (
-                <>
-                  <div key={item._id} className={styles.product}>
-                    <div className={styles.image}>
+        {items && items.length >= 1
+          ? (
+            <div className={styles.cartThing}>
+              <div className={styles.cartItems}>
+                {items.map((item) => (
+                  <>
+                    <div className={styles.product}>
                       <img src={item.image} alt={item.name} />
+                      <div className={styles.info}>
+                        <h5>{item.name}</h5>
+                        <h6 className={styles.description}>{item.description}</h6>
+                        <h6 className={styles.price}>
+                          $
+                          {item.price}
+                        </h6>
+                      </div>
                     </div>
-                    <div className={styles.info}>
-                      <h5>{item.name}</h5>
-                      <h6 className={styles.description}>{item.description}</h6>
-                      <h6 className={styles.price}>
-                        $
-                        {item.price}
-                      </h6>
-                    </div>
-                  </div>
-                  <hr />
-                </>
-              ))}
-              <div className={styles.total}>
+                    <hr />
+                  </>
+                ))}
+              </div>
+              <div>
                 <Total />
               </div>
             </div>
+          )
+          : (
+            <h4 className={styles.empty}>Cart is empty...</h4>
           )}
       </div>
     </div>

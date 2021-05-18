83d97da7b6e5b9b1c98087857252258ee9e60cf5
Alan Sanchez <realalansan@gmail.com>
2021-05-18 16:44:24 -0700

    Build project

--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -7,3 +7,5 @@ export const FETCH_PRODUCT = 'FETCH_PRODUCT';
 export const FETCH_CART = 'FETCH_CART';
 export const ADD_TO_CART = 'ADD_TO_CART';
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART';
+export const ADD_AMOUNT = 'ADD_AMOUNT';
+export const SUBTRACT_AMOUNT = 'SUBTRACT_AMOOUNT';
